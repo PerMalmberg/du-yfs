f120fdf36c944351cbf6c6ed5a9bf50d415d324f
Per Malmberg <per.malmberg@gmail.com>
2022-02-03 18:35:50 +0100

    Comes to a stand still.

--- src/Accelerator.lua
+++ src/Accelerator.lua
@@ -10,6 +10,7 @@ acc.__index = acc
 
 local Mode =
     Enum {
+    "IDLE",
     "ACC",
     "MOVE_RAMP_UP",
     "MOVE_RAMP_DOWN"
@@ -18,6 +19,7 @@ local Mode =
 local function new()
     local instance = {
         targetSpeed = 0,
+        initialDirection = 0,
         acceleration = 0,
         forwardDirection = 0,
         mode = Mode.ACC
@@ -31,12 +33,14 @@ end
 function acc:AccelerateTo(currentSpeed, targetSpeed, seconds, forwardDirection)
     self.mode = Mode.ACC
     self.targetSpeed = abs(targetSpeed)
+    self.initialDirection = calc.Sign(currentSpeed)
     self.acceleration = calc.AbsDiff(targetSpeed, currentSpeed) / seconds
     self.forwardDirection = forwardDirection
 end
 
 function acc:MoveDistance(currentSpeed, distance, time, forwardDirection)
     self.mode = Mode.MOVE_RAMP_UP
+    self.initialDirection = calc.Sign(currentSpeed)
     self.forwardDirection = forwardDirection
     -- Make two-part acceleration, ramp-up then ramp-down
     -- S = V0*t + 0.5*a*t^2
@@ -50,7 +54,7 @@ end
 
 local function accelerateToSpeed(targetSpeed, currentSpeed, acceleration, forwardDirection)
     local delta = targetSpeed - abs(currentSpeed)
-    system.print(targetSpeed .. "  " .. currentSpeed .. "  " .. delta)
+
     if delta > 0 then
         -- Accelerate
         return acceleration * forwardDirection
@@ -61,22 +65,36 @@ local function accelerateToSpeed(targetSpeed, currentSpeed, acceleration, forwar
 end
 
 function acc:Feed(currentSpeed)
+    local absSpeed = abs(currentSpeed)
     if self.mode == Mode.ACC then
-        return accelerateToSpeed(self.targetSpeed, abs(currentSpeed), self.acceleration, self.forwardDirection)
+        return accelerateToSpeed(self.targetSpeed, absSpeed, self.acceleration, self.forwardDirection)
     elseif self.mode == Mode.MOVE_RAMP_UP then
-        if abs(currentSpeed) >= self.targetSpeed then
+        system.print("UP")
+        if absSpeed >= self.targetSpeed then
             self.mode = Mode.MOVE_RAMP_DOWN
             self.targetSpeed = 0
         else
-            return accelerateToSpeed(self.targetSpeed, abs(currentSpeed), self.acceleration, self.forwardDirection)
+            return accelerateToSpeed(self.targetSpeed, absSpeed, self.acceleration, self.forwardDirection)
         end
     elseif self.mode == Mode.MOVE_RAMP_DOWN then
-        if abs(currentSpeed) > 0 then
-            return accelerateToSpeed(self.targetSpeed, abs(currentSpeed), self.acceleration, self.forwardDirection)
+        local speedNextTick = currentSpeed + self.acceleration * constants.flushTick
+        local nextDir = calc.Sign(speedNextTick)
+
+        system.print(calc.Sign(speedNextTick) .. " " .. calc.Sign(self.initialDirection))
+        system.print("DOWN1")
+
+        if nextDir == self.forwardDirection then
+            system.print("DOWN2")
+            return accelerateToSpeed(self.targetSpeed, absSpeed, self.acceleration, self.forwardDirection)
         else
             -- Reached destiation
+            self.mode = Mode.IDLE
+            system.print("Idle")
             return 0
         end
+    else
+        -- Idle
+        return 0
     end
 end
 
