ba949bd5cf98c639d0c556441e1ebbe12a43d4b4
Per Malmberg <per.malmberg@gmail.com>
2022-06-18 16:15:11 +0200

    Keeps direction when waypoint is above/below.

--- src/flight/AlignmentFunctions.lua
+++ src/flight/AlignmentFunctions.lua
@@ -1,5 +1,6 @@
 local construct = require("du-libs:abstraction/Construct")()
 local universe = require("du-libs:universe/Universe")()
+local abs = math.abs
 
 local alignment = {}
 
@@ -12,6 +13,14 @@ end
 function alignment.YawPitchKeepOrthogonalToGravity(waypoint, previousWaypoint)
     local normal = -construct.world.GAlongGravity():normalize_inplace()
     local dir = (waypoint.destination - construct.position.Current()):normalize_inplace()
+
+    if abs(dir:dot(normal)) > 0.9 then
+        -- When the next waypoint is nearly above or below us, switch alignment mode.
+        -- This 'trick' allows turning also in manual control
+        waypoint:OneTimeSetYawPitchDirection(construct.orientation.Forward(), alignment.YawPitchKeepWaypointDirectionOrthogonalToGravity)
+        return nil
+    end
+
     dir = dir:project_on_plane(normal)
     return construct.position.Current() + dir * 10
 end
