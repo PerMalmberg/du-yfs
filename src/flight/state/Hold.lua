bdebcf2046656a3c23b0f508c6311b5889831079
Per Malmberg <per.malmberg@gmail.com>
2022-07-20 15:38:19 +0200

    Let regular deviation adjustment hold construct in Hold state.

--- src/flight/state/Hold.lua
+++ src/flight/state/Hold.lua
@@ -31,20 +31,12 @@ function state:Leave()
 end
 
 function state:Flush(next, previous, chaseData)
+    self.fsm:Thrust()
+
     if next:Reached() then
         next:SetPrecisionMode(true)
         brakes:Set(true)
-
-        -- If not moving towards the desired point, counter movement
-        local vel = Velocity()
-        local dir = vel:normalize()
-        local diff = (chaseData.rabbit - CurrentPos()):normalize_inplace()
-
-        if diff:dot(dir) < 0.707 then
-            self.fsm:Thrust(-Velocity():normalize_inplace() * 0.5)
-        end
     else
-        self.fsm:Thrust()
         self.fsm:SetState(ApproachWaypoint(self.fsm))
     end
 end
