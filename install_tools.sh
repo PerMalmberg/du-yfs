91ae1ea525e4edbdb50527fe06ba1eca4eaf2d40
Per Malmberg <per.malmberg@gmail.com>
2022-09-17 17:28:01 +0200

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -1,6 +1,11 @@
 set -e -o pipefail
 
-sudo apt install lua5.3 liblua5.3-dev luarocks
-sudo luarocks install busted
-sudo luarocks install luacov
-sudo luarocks install luacov-html
+sudo apt install npm sudo apt lua5.3 liblua5.3-dev luarocks -y
+luarocks --local install busted
+luarocks --local install luacov
+luarocks --local install luacov-html
+cd ~
+npm i @wolfe-labs/du-luac
+
+echo 
+echo Add this to your PATH: ${HOME}/.luarocks/bin
