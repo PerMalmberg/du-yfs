8fc2a2f74425ab774c63f0e8689317ca24490c3f
Per Malmberg <per.malmberg@gmail.com>
2022-09-17 17:46:36 +0200

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -4,8 +4,10 @@ sudo apt install npm sudo apt lua5.3 liblua5.3-dev luarocks -y
 luarocks --local install busted
 luarocks --local install luacov
 luarocks --local install luacov-html
-cd ~
-npm i @wolfe-labs/du-luac
+# Install globally to keep node_modules away from project directory
+sudo npm i -g @wolfe-labs/du-luac
+# Give us ownership of node_modules
+sudo chown -R $USER /usr/local/lib/node_modules
 
 echo 
 echo Add this to your PATH: ${HOME}/.luarocks/bin
