4b56f55c061d0abfdb624a5d73f6f2d19520f0c5
Per Malmberg <per.malmberg@gmail.com>
2023-02-18 19:55:54 +0100

    Small adjustement to adjustAccLookup table.

--- src/flight/FlightFSM.lua
+++ src/flight/FlightFSM.lua
@@ -19,30 +19,30 @@ local Stopwatch = require("system/Stopwatch")
 local PID = require("cpml/pid")
 local Ray = require("util/Ray")
 require("flight/state/Require")
-local CurrentPos       = vehicle.position.Current
-local Velocity         = vehicle.velocity.Movement
-local Acceleration     = vehicle.acceleration.Movement
-local GravityDirection = vehicle.world.GravityDirection
-local AtmoDensity      = vehicle.world.AtmoDensity
-local TotalMass        = vehicle.mass.Total
-local utils            = require("cpml/utils")
-local pub              = require("util/PubSub").Instance()
-local clamp            = utils.clamp
-local abs              = math.abs
-local min              = math.min
-local max              = math.max
-local MAX_INT          = math.maxinteger
+local CurrentPos                    = vehicle.position.Current
+local Velocity                      = vehicle.velocity.Movement
+local Acceleration                  = vehicle.acceleration.Movement
+local GravityDirection              = vehicle.world.GravityDirection
+local AtmoDensity                   = vehicle.world.AtmoDensity
+local TotalMass                     = vehicle.mass.Total
+local utils                         = require("cpml/utils")
+local pub                           = require("util/PubSub").Instance()
+local clamp                         = utils.clamp
+local abs                           = math.abs
+local min                           = math.min
+local max                           = math.max
+local MAX_INT                       = math.maxinteger
 
 local ignoreAtmoBrakeLimitThreshold = calc.Kph2Mps(3)
 
-local longitudinal = "longitudinal"
-local vertical = "vertical"
-local lateral = "lateral"
-local airfoil = "airfoil"
-local thrustTag = "thrust"
-local Forward = vehicle.orientation.Forward
-local Right = vehicle.orientation.Right
-local deadZoneFactor = 0.8 -- Consider the inner edge of the dead zone where we can't brake to start at this percentage of the atmosphere.
+local longitudinal                  = "longitudinal"
+local vertical                      = "vertical"
+local lateral                       = "lateral"
+local airfoil                       = "airfoil"
+local thrustTag                     = "thrust"
+local Forward                       = vehicle.orientation.Forward
+local Right                         = vehicle.orientation.Right
+local deadZoneFactor                = 0.8 -- Consider the inner edge of the dead zone where we can't brake to start at this percentage of the atmosphere.
 
 ---@class FlightFSM
 ---@field New fun(settings:Settings):FlightFSM
@@ -58,15 +58,14 @@ local deadZoneFactor = 0.8 -- Consider the inner edge of the dead zone where we 
 ---@field SetFlightCore fun(fc:FlightCore)
 ---@field GetFlightCore fun():FlightCore
 
-local FlightFSM = {}
-FlightFSM.__index = FlightFSM
+local FlightFSM                     = {}
+FlightFSM.__index                   = FlightFSM
 
 ---Creates a new FligtFSM
 ---@param settings Settings
 ---@param routeController RouteController
 ---@return FlightFSM
 function FlightFSM.New(settings, routeController)
-
     local function antiG()
         return -universe:VerticalReferenceVector() * G()
     end
@@ -103,11 +102,12 @@ function FlightFSM.New(settings, routeController)
     }
 
     local adjustAccLookup = {
-        { limit = 0, acc = 0.15, reverse = 0.3 },
-        { limit = 0.01, acc = 0.20, reverse = 0.35 },
-        { limit = 0.03, acc = 0.30, reverse = 0.40 },
-        { limit = 0.05, acc = 0.40, reverse = 0.45 },
-        { limit = 0.1, acc = 0, reverse = 0 }
+        { limit = 0,    acc = 0.001, reverse = 0.001 },
+        { limit = 0.05, acc = 0.20,  reverse = 0.4 },
+        { limit = 0.1,  acc = 0.30,  reverse = 0.7 },
+        { limit = 0.15, acc = 0.40,  reverse = 0.8 },
+        { limit = 0.2,  acc = 0.40,  reverse = 1 },
+        { limit = 0.25, acc = 0,     reverse = 0 }
     }
 
     local toleranceDistance = 2 -- meters. This limit affects the steepness of the acceleration curve used by the deviation adjustment
@@ -147,15 +147,18 @@ function FlightFSM.New(settings, routeController)
     local function getAdjustedAcceleration(accLookup, distance, movingTowardsTarget)
         local selected
         for _, v in ipairs(accLookup) do
-            if distance >= v.limit then selected = v
-            else break end
+            if distance >= v.limit then
+                selected = v
+            else
+                break
+            end
         end
 
         if selected.acc == 0 then
-            return accLookup[#accLookup - 1].acc
-        else
-            return calc.Ternary(movingTowardsTarget, selected.acc, selected.reverse)
+            selected = accLookup[#accLookup - 1]
         end
+
+        return calc.Ternary(movingTowardsTarget, selected.acc, selected.reverse)
     end
 
     local function getEngines(moveDirection, precision)
@@ -394,7 +397,6 @@ function FlightFSM.New(settings, routeController)
         if inAtmo and not willLeaveAtmo and waypoint.FinalSpeed() <= brakeDegradeSpeed then
             local tenPercent = brakes.MaxSeenGravityInfluencedAvailableAtmoDeceleration() * 0.1
             if tenPercent > 0 then
-
                 local endSpeed = waypoint.FinalSpeed()
 
                 local finalApproachDistance = calc.CalcBrakeDistance(brakeDegradeSpeed, tenPercent)
@@ -403,14 +405,14 @@ function FlightFSM.New(settings, routeController)
                 if toBrakePoint > 0 then
                     -- Not yet reached the break point
                     targetSpeed = evaluateNewLimit(targetSpeed,
-                        calcMaxAllowedSpeed(-tenPercent, toBrakePoint, brakeDegradeSpeed),
+                        calcMaxAllowedSpeed( -tenPercent, toBrakePoint, brakeDegradeSpeed),
                         "Appr. fin.")
                     flightData.finalSpeed = brakeDegradeSpeed
                     flightData.finalSpeedDistance = toBrakePoint
                 else
                     -- Within
                     targetSpeed = evaluateNewLimit(targetSpeed,
-                        calcMaxAllowedSpeed(-tenPercent, remainingDistance, endSpeed),
+                        calcMaxAllowedSpeed( -tenPercent, remainingDistance, endSpeed),
                         "Final")
                 end
             end
@@ -457,7 +459,7 @@ function FlightFSM.New(settings, routeController)
 
         local movingTowardsTarget = deviationAccum:Add(vel:Normalize():Dot(dirToTarget) > 0.8) > 0.5
 
-        local maxBrakeAcc = engine:GetMaxPossibleAccelerationInWorldDirectionForPathFollow(-toTargetWorld:Normalize(),
+        local maxBrakeAcc = engine:GetMaxPossibleAccelerationInWorldDirectionForPathFollow( -toTargetWorld:Normalize(),
             false)
         local brakeDistance = CalcBrakeDistance(currSpeed, maxBrakeAcc) + warmupTime * currSpeed
         local speedLimit = calc.Scale(distance, 0, toleranceDistance, adjustmentSpeedMin, adjustmentSpeedMax)
@@ -652,7 +654,6 @@ function FlightFSM.New(settings, routeController)
         --[[ As waypoints can have large margins, we need to ensure that we allow for offsets as large as the margins, at each end.
             The outer edges are a straight line between the edges of the start and end point spheres so allowed offset can be calculated linearly.
         ]]
-
         local dist = (previousWaypoint.Destination() - nextWaypoint.Destination()):Len()
         local diff = previousWaypoint.Margin() - nextWaypoint.Margin()
         local koeff = diff / dist
